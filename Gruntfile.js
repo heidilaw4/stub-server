deb2aa700ec9513fd514f3c4a49a41a07540f9fd
semykin <v.v.semykin@gmail.com>
2015-05-16 23:46:33 +0300

    Configure grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,49 @@
 module.exports = function (grunt) {
 
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                src: ['tests/**/*.js']
+            }
+        },
 
+        watch: {
+            tests: {
+                files: ['tests/**/*.js'],
+                tasks: ['mochaTest'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+
+        concurrent: {
+            dev: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+
+        nodemon: {
+            dev: {
+                script: 'examples/app.js',
+                options: {
+                    ignore: ['node_modules/**', 'examples/**', 'tests/**']
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['concurrent:dev']);
 };
