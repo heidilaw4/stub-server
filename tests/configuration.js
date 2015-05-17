8c354cca790c6f09e46df24cc8ffd6c7fc1ad7ed
semykin <v.v.semykin@gmail.com>
2015-05-17 15:49:23 +0300

    Adding MESSAGES object for logging and exceptions

--- tests/configuration.js
+++ tests/configuration.js
@@ -1,6 +1,7 @@
 var should = require('should'),
         configuration = require('./../lib/configuration'),
-        Deferred = require('./../lib/deferred');
+        Deferred = require('./../lib/deferred'),
+        MESSAGES = require('./../lib/MESSAGES');
 
 describe('Configuration', function () {
 
@@ -12,29 +13,29 @@ describe('Configuration', function () {
 
         it('when no configuration object or has no stub property', function () {
 
-            config().should.throw('Wrong configuration');
-            config({}).should.throw('Wrong configuration');
-            config({ stubs: true }).should.not.throw('Wrong configuration');
+            config().should.throw(MESSAGES.ERRORS.WRONG_CONFIGURATION);
+            config({}).should.throw(MESSAGES.ERRORS.WRONG_CONFIGURATION);
+            config({ stubs: true }).should.not.throw(MESSAGES.ERRORS.WRONG_CONFIGURATION);
         });
 
         it('when incorrect stubs type', function () {
-            config({ stubs: true }).should.throw('Wrong stubs type');
-            config({ stubs: 1 }).should.throw('Wrong stubs type');
-            config({ stubs: [] }).should.not.throw('Wrong stubs type');
-            config({ stubs: ' ' }).should.not.throw('Wrong stubs type');
-            config({ stubs: {} }).should.throw('Wrong stubs type');
+            config({ stubs: true }).should.throw(MESSAGES.ERRORS.WRONG_STUB_TYPE);
+            config({ stubs: 1 }).should.throw(MESSAGES.ERRORS.WRONG_STUB_TYPE);
+            config({ stubs: [] }).should.not.throw(MESSAGES.ERRORS.WRONG_STUB_TYPE);
+            config({ stubs: ' ' }).should.not.throw(MESSAGES.ERRORS.WRONG_STUB_TYPE);
+            config({ stubs: {} }).should.throw(MESSAGES.ERRORS.WRONG_STUB_TYPE);
         });
 
         describe('If stubs property is string', function () {
 
             it('should throw exception if no such file', function () {
-                config({ stubs: 'wrong-file-config.json' }).should.throw('Wrong path to stubs configuration');
-                config({ stubs: 'tests/configs/empty-config.json' }).should.not.throw('Wrong path to stubs configuration');
+                config({ stubs: 'wrong-file-config.json' }).should.throw(MESSAGES.ERRORS.WRONG_CONFIGURATION_PATH);
+                config({ stubs: 'tests/configs/empty-config.json' }).should.not.throw(MESSAGES.ERRORS.WRONG_CONFIGURATION_PATH);
             });
 
             it('if file format is not JSON - throw an exception', function () {
-                config({ stubs: 'tests/configs/empty-config.json' }).should.throw('Invalid JSON format for stubs configuration');
-                config({ stubs: 'tests/configs/empty-json-config.json' }).should.not.throw('Invalid JSON format for stubs configuration');
+                config({ stubs: 'tests/configs/empty-config.json' }).should.throw(MESSAGES.ERRORS.INVALID_STUB_JSON_FORMAT);
+                config({ stubs: 'tests/configs/empty-json-config.json' }).should.not.throw(MESSAGES.ERRORS.INVALID_STUB_JSON_FORMAT);
             });
 
         });
